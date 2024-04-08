c00e41c72fc0358c5c7dab3d3f28304364327608
Franklin Shera <fshera96@gmail.com>
2024-04-08 20:53:37 +0300

    extract deploy function

--- src/routes/dashboard/[template]/[project]/index.tsx
+++ src/routes/dashboard/[template]/[project]/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useComputed$, useSignal } from "@builder.io/qwik";
+import { $, component$, useComputed$, useSignal } from "@builder.io/qwik";
 import { toast } from "qwik-sonner";
 // import { useQwikTable } from "@tanstack/qwik-table";
 
@@ -49,77 +49,89 @@ export default component$(() => {
     );
   }
 
+  const deploy = $(async () => {
+    isDeploying.value = true;
+
+    streamResponse.value = [];
+
+    const res = await t.value.handler({
+      WORKING_DIR: project.value.id,
+    });
+
+    for await (const i of res) {
+      if (i.type === "START") {
+        toast("🚀 " + i.value);
+      }
+
+      if (i.type === "END") {
+        toast.success(i.value);
+      }
+
+      if (i.type === "INFO") {
+        toast.info(i.value);
+      }
+
+      if (i.type === "ERROR") {
+        toast.error(i.value);
+      }
+
+      streamResponse.value = [...streamResponse.value, i];
+    }
+
+    isDeploying.value = false;
+  });
+
   return (
     <div class="flex flex-col gap-5 ">
-      <div class="space-y-3">
-        <h2 class="text-2xl capitalize">{project.value.name}</h2>
-        <button
-          class="flex items-center gap-1 rounded-lg bg-black p-2 pl-4 pr-5 text-white disabled:cursor-not-allowed disabled:text-white/70"
-          disabled={isDeploying.value}
-          onClick$={async () => {
-            isDeploying.value = true;
-
-            streamResponse.value = [];
-
-            const res = await t.value.handler({
-              WORKING_DIR: project.value.id,
-            });
-
-            for await (const i of res) {
-              if (i.type === "START") {
-                toast("🚀 " + i.value);
-              }
-
-              if (i.type === "END") {
-                toast.success(i.value);
-              }
-
-              if (i.type === "INFO") {
-                toast.info(i.value);
-              }
-
-              if (i.type === "ERROR") {
-                toast.error(i.value);
-              }
-
-              streamResponse.value = [...streamResponse.value, i];
-            }
-
-            isDeploying.value = false;
-
-            console.log("END");
-          }}
-        >
-          {isDeploying.value ? (
-            <svg
-              class="h-auto w-5 shrink-0 animate-spin"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 15 15"
-            >
-              <path
-                fill="currentColor"
-                fill-rule="evenodd"
-                d="M1.85 7.5c0-2.835 2.21-5.65 5.65-5.65c2.778 0 4.152 2.056 4.737 3.15H10.5a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-1 0v1.813C12.296 3.071 10.666.85 7.5.85C3.437.85.85 4.185.85 7.5c0 3.315 2.587 6.65 6.65 6.65c1.944 0 3.562-.77 4.714-1.942a6.77 6.77 0 0 0 1.428-2.167a.5.5 0 1 0-.925-.38a5.77 5.77 0 0 1-1.216 1.846c-.971.99-2.336 1.643-4.001 1.643c-3.44 0-5.65-2.815-5.65-5.65"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          ) : (
-            <svg
-              class="h-auto w-5 shrink-0"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <circle cx="12" cy="12" r="10"></circle>
-              <polygon points="10 8 16 12 10 16 10 8"></polygon>
-            </svg>
-          )}
-          {isDeploying.value ? "Deploying..." : "Deploy"}
-        </button>
+      <div class="flex justify-between">
+        <div>
+          <h2 class="text-2xl capitalize">{project.value.name}</h2>
+          <span class="inline-block rounded-full border border-current px-2 py-1 text-sm text-gray-600">
+            {project.value.id}
+          </span>
+        </div>
+        <div>
+          <button
+            class="flex items-center gap-1 rounded-xl bg-black p-2.5 pl-4 pr-5 text-white disabled:cursor-not-allowed disabled:text-white/70"
+            disabled={isDeploying.value}
+            onClick$={deploy}
+          >
+            {isDeploying.value ? (
+              <>
+                <svg
+                  class="h-auto w-5 shrink-0 animate-spin"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 15 15"
+                >
+                  <path
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    d="M1.85 7.5c0-2.835 2.21-5.65 5.65-5.65c2.778 0 4.152 2.056 4.737 3.15H10.5a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-1 0v1.813C12.296 3.071 10.666.85 7.5.85C3.437.85.85 4.185.85 7.5c0 3.315 2.587 6.65 6.65 6.65c1.944 0 3.562-.77 4.714-1.942a6.77 6.77 0 0 0 1.428-2.167a.5.5 0 1 0-.925-.38a5.77 5.77 0 0 1-1.216 1.846c-.971.99-2.336 1.643-4.001 1.643c-3.44 0-5.65-2.815-5.65-5.65"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Deploying...
+              </>
+            ) : (
+              <>
+                <svg
+                  class="h-auto w-5 shrink-0"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <circle cx="12" cy="12" r="10"></circle>
+                  <polygon points="10 8 16 12 10 16 10 8"></polygon>
+                </svg>
+                Deploy
+              </>
+            )}
+          </button>
+        </div>
       </div>
       <hr />
       <div class="flex-1 rounded bg-[#111] px-3 py-5">
