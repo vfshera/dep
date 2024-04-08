6c1df47ee5ae2fc6ce26cb3fe3576bc086328bca
Franklin Shera <fshera96@gmail.com>
2024-04-08 15:42:32 +0300

    types

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+import { type ServerQRL } from "@builder.io/qwik-city";
+
+export type DeployScript<T = {}> = {
+  id: string;
+  name: string;
+  description: string;
+  handler: ServerQRL<
+    (
+      args: { WORKING_DIR: string } & T,
+    ) => AsyncGenerator<ScriptYield, void, unknown>
+  >;
+};
+export type ScriptYield = {
+  type: "INFO" | "DATA" | "ERROR" | "WARN" | "END" | "START";
+  value: string;
+};
