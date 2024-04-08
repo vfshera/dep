428987502098194f3e42a6f3b2773ef7897825c3
Franklin Shera <fshera96@gmail.com>
2024-04-08 18:00:19 +0300

    link

--- src/routes/dashboard/Nav.tsx
+++ src/routes/dashboard/Nav.tsx
@@ -1,4 +1,5 @@
 import { component$, useSignal } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
   const links = useSignal([
@@ -17,7 +18,9 @@ export default component$(() => {
   ]);
   return (
     <header class="flex h-12 items-center justify-between border-b px-5">
-      <h1 class="text-xl font-bold tracking-tight">Dashboard</h1>
+      <Link href="/dashboard" class="text-xl font-bold tracking-tight">
+        Dashboard
+      </Link>
       <ul class="flex items-center gap-6">
         {links.value.map((link, index) => (
           <li key={index}>
