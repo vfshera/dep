efc58e19cc00c075beb17ac16647e7cc45e694a9
Franklin Shera <fshera96@gmail.com>
2024-04-09 22:22:50 +0300

    clear links

--- src/routes/dashboard/Nav.tsx
+++ src/routes/dashboard/Nav.tsx
@@ -1,35 +1,18 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
+import { useAuthSession } from "../plugin@auth";
 
 export default component$(() => {
-  const links = useSignal([
-    {
-      name: "Link",
-      href: "#",
-    },
-    {
-      name: "Link 2",
-      href: "#",
-    },
-    {
-      name: "Link 3",
-      href: "#",
-    },
-  ]);
+  const session = useAuthSession();
   return (
     <header class="flex h-12 items-center justify-between border-b px-5">
       <Link href="/dashboard" class="text-xl font-bold tracking-tight">
         Dashboard
       </Link>
-      <ul class="flex items-center gap-6">
-        {links.value.map((link, index) => (
-          <li key={index}>
-            <a href={link.href} class="text-sm">
-              {link.name}
-            </a>
-          </li>
-        ))}
-      </ul>
+
+      <div>
+        <p>{session.value?.user?.name}</p>
+      </div>
     </header>
   );
 });
