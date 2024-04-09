fdf838ebbb34770dbb807495cab87a7aaa97db65
Franklin Shera <fshera96@gmail.com>
2024-04-09 22:20:36 +0300

    add logger and utils

---
+++ src/utils/logger.ts
@@ -0,0 +1,69 @@
+import * as winston from "winston";
+
+import DailyRotateFile from "winston-daily-rotate-file";
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+};
+
+const format = winston.format.combine(
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+  winston.format.json(),
+);
+
+export function createWinstonLogger(options: winston.LoggerOptions = {}) {
+  return winston.createLogger({
+    level: "info",
+    levels,
+    format,
+    transports: [
+      createDailyRotateFileTransport(),
+      new winston.transports.Console(),
+    ],
+    exitOnError: false,
+    exceptionHandlers: [
+      new winston.transports.File({
+        filename: "exceptions.log",
+        dirname: "logs",
+      }),
+    ],
+    ...options,
+  });
+}
+
+export function createDailyRotateFileTransport(
+  options: DailyRotateFile.DailyRotateFileTransportOptions = {},
+) {
+  return new DailyRotateFile({
+    filename: "%DATE%.log",
+    datePattern: "YYYY-MM-DD-HH",
+    zippedArchive: true,
+    maxSize: "10m",
+    maxFiles: "14d",
+    dirname: "logs",
+    ...options,
+  });
+}
+
+export function scriptLogger(options: { id: string }) {
+  return createWinstonLogger({
+    level: "end",
+    levels: { ...levels, data: 3, start: 4, end: 5 },
+    transports: [
+      createDailyRotateFileTransport({
+        dirname: `logs/${options.id}`,
+        json: true,
+      }),
+    ],
+    exceptionHandlers: [
+      new winston.transports.File({
+        filename: "exceptions.log",
+        dirname: `logs/${options.id}`,
+      }),
+    ],
+  });
+}
+
+export default createWinstonLogger();
