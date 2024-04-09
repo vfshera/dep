77bcd976e0baed305c91262588bfe377948249d0
Franklin Shera <fshera96@gmail.com>
2024-04-09 15:43:33 +0300

    block users by mail

--- src/routes/dashboard/layout.tsx
+++ src/routes/dashboard/layout.tsx
@@ -1,7 +1,25 @@
 import { Slot, component$ } from "@builder.io/qwik";
 import Nav from "./Nav";
-import { type DocumentHead } from "@builder.io/qwik-city";
+import { type RequestHandler, type DocumentHead } from "@builder.io/qwik-city";
 import { Toaster } from "qwik-sonner";
+import { type Session } from "@auth/core/types";
+
+export const onRequest: RequestHandler = (event) => {
+  const session: Session | null = event.sharedMap.get("session");
+
+  const ALLOWED_USERS = event.env.get("ALLOWED_USERS")?.split(",");
+
+  if (!session || new Date(session.expires) < new Date() || !ALLOWED_USERS) {
+    throw event.redirect(
+      302,
+      `/api/auth/signin?callbackUrl=${event.url.pathname}`,
+    );
+  }
+
+  if (!session.user?.email || !ALLOWED_USERS.includes(session.user.email)) {
+    throw event.redirect(302, "/");
+  }
+};
 
 export default component$(() => {
   return (
