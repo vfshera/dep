c1139a5f2643d428fbc8e75b148f0fdddc726afe
Franklin Shera <fshera96@gmail.com>
2024-04-10 21:35:09 +0300

    fixes

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -9,7 +9,7 @@ export default component$(() => {
     <div class="flex flex-col gap-5 p-5">
       <h2>Deployment Templates</h2>
 
-      <ul class="grid grid-cols-4">
+      <ul class="grid grid-cols-4 gap-2">
         {scripts.map((s) => (
           <li key={s.id} class="min-h-[16rem]">
             <Link href={`/dashboard/${s.id}`} class="block h-full w-full">
