f9041fd92d0711df063b25743a3c96ac4c2a6783
Franklin Shera <fshera96@gmail.com>
2024-04-20 07:59:35 +0300

    consts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,7 @@
 export const WORKING_DIR_KEY = "CMD_WORKING_DIR";
 
 export const DEPLOY_SCRIPT_NAME = "dep.sh";
+
+export const DEPLOY_DIR_NAME = ".dep";
+
+export const DEPLOY_FILE_NAME = ["deploy.yml", "deploy.yaml"];
