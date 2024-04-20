5856e04cdb4d16b1a2576c4d7034ce2db5fcb083
Franklin Shera <fshera96@gmail.com>
2024-04-20 07:58:55 +0300

    db credentials

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,12 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import schema from "./schema";
 import postgres from "postgres";
 
-const connection = postgres();
+const connection = postgres({
+  database: process.env.DB_NAME as string,
+  host: process.env.DB_HOST as string,
+  user: process.env.DB_USER as string,
+  password: process.env.DB_PASS as string,
+});
 
 const db = drizzle(connection, { schema });
 
