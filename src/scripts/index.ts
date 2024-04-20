e545cd7056901e536484eee67d4a3728514a921c
Franklin Shera <fshera96@gmail.com>
2024-04-20 08:02:06 +0300

    cleanup

--- src/scripts/index.ts
+++
@@ -1,8 +0,0 @@
-import astro from "./astro";
-import depBash from "./dep-bash";
-
-const scripts = [depBash(), astro()];
-
-const uniqueScriptIdsids = [...new Set(scripts.map((s) => s.id))];
-
-export default scripts.filter((s) => uniqueScriptIdsids.includes(s.id));
