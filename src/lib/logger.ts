7252b32f2e3b8d2161d5ca0880905086f0e4392c
Franklin Shera <fshera96@gmail.com>
2024-04-20 08:01:12 +0300

    update logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,4 +1,5 @@
 import * as winston from "winston";
+import { type ScriptYield } from "~/types";
 
 import DailyRotateFile from "winston-daily-rotate-file";
 
@@ -65,5 +66,39 @@ export function scriptLogger(options: { id: string }) {
     ],
   });
 }
+
+/**
+ *  
+ */
+
+
+export function info(msg: string): ScriptYield {
+  return { type: "INFO", value: msg };
+}
+
+export function data(msg: string): ScriptYield {
+  return { type: "DATA", value: msg };
+}
+
+export function error(msg: string): ScriptYield {
+  return { type: "ERROR", value: msg };
+}
+
+export function success(msg: string): ScriptYield {
+  return { type: "SUCCESS", value: msg };
+}
+
+export function warn(msg: string): ScriptYield {
+  return { type: "WARN", value: msg };
+}
+
+export function end(): ScriptYield {
+  return { type: "END", value: "" };
+}
+
+export function start(msg: string): ScriptYield {
+  return { type: "START", value: msg };
+}
+
 
 export default createWinstonLogger();
