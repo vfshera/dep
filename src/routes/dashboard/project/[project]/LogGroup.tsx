4604d02eb6fe53f4e1a30f65d8216f80a3065f6e
Franklin Shera <fshera96@gmail.com>
2024-07-03 01:39:46 +0300

    minimized logs

--- src/routes/dashboard/project/[project]/LogGroup.tsx
+++ src/routes/dashboard/project/[project]/LogGroup.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import { cn, type PrettyLogsOutput } from "~/utils";
 
 type LogGroupProps = {
@@ -6,42 +6,93 @@ type LogGroupProps = {
 };
 
 export default component$<LogGroupProps>(({ logGroup }) => {
+  const open = useSignal(false);
+
   return (
-    <div
-      key={`${logGroup.timestamp.raw}`}
-      class="rounded bg-dark-2  p-1 pt-2.5 first:pt-0 "
-    >
-      <span class="inline-block bg-dark-3 p-2 text-sm text-white">
-        Run: {logGroup.timestamp.relative}
-      </span>
-      <ul class="border-l-2 border-dark-3 py-2 pl-2">
-        {logGroup.items
-          .filter((l) => l.level !== "end")
-          .map((buildLog, i) => (
-            <li
-              key={i + buildLog.timestamp.raw}
-              class={cn("flex gap-3", buildLog.level === "info" && "mt-2")}
+    <div onClick$={() => (open.value = !open.value)}>
+      <div
+        class={[
+          "grid transition-[grid-template-rows] duration-300 ease-in-out",
+          open.value ? "grid-rows-[0fr]" : "grid-rows-[1fr]",
+        ]}
+      >
+        <div class="overflow-hidden bg-dark-2">
+          <div>
+            <span
+              class={["inline-block bg-dark-3 px-2 py-1.5 text-sm text-white"]}
             >
-              <span class="shrink-0 text-sm text-white/90">
-                {buildLog.timestamp.raw}
-              </span>
-              <pre
-                class={cn(
-                  "block cursor-pointer whitespace-break-spaces  break-words rounded  px-2 py-0.5 text-sm text-white hover:bg-white/5 hover:transition-colors",
-                  buildLog.level === "info" && "font-medium text-blue-400",
-                  buildLog.level === "error" && "text-red-500",
-                  buildLog.level === "success" && "text-green-500",
-                )}
+              {logGroup.timestamp.relative}
+            </span>
+            <div class="flex items-center gap-2 p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-5"
               >
-                {buildLog.level === "start" && "🚀 "}
-                {buildLog.level === "success" && "✅ "}
-                {buildLog.level === "info"
-                  ? `[${buildLog.message}]`
-                  : buildLog.message}
-              </pre>
-            </li>
-          ))}
-      </ul>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+              {logGroup.timestamp.raw}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class={[
+          "grid transition-[grid-template-rows] duration-500 ease-in-out",
+          open.value ? "grid-rows-[1fr]" : "grid-rows-[0fr]",
+        ]}
+      >
+        <div class="overflow-hidden">
+          <div
+            key={`${logGroup.timestamp.raw}`}
+            class=" rounded  bg-dark-2 p-1 pt-2.5 first:pt-0"
+          >
+            <span class="inline-block bg-dark-3 p-2 text-sm text-white">
+              Run: {logGroup.timestamp.relative}
+            </span>
+            <ul class="border-l-2 border-dark-3 py-2 pl-2">
+              {logGroup.items
+                .filter((l) => l.level !== "end")
+                .map((buildLog, i) => (
+                  <li
+                    key={i + buildLog.timestamp.raw}
+                    class={cn(
+                      "flex gap-3",
+                      buildLog.level === "info" && "mt-2",
+                    )}
+                  >
+                    <span class="shrink-0 text-sm text-white/90">
+                      {buildLog.timestamp.raw}
+                    </span>
+                    <pre
+                      class={cn(
+                        "block cursor-pointer whitespace-break-spaces  break-words rounded  px-2 py-0.5 text-sm text-white hover:bg-white/5 hover:transition-colors",
+                        buildLog.level === "info" &&
+                          "font-medium text-blue-400",
+                        buildLog.level === "error" && "text-red-500",
+                        buildLog.level === "success" && "text-green-500",
+                      )}
+                    >
+                      {buildLog.level === "start" && "🚀 "}
+                      {buildLog.level === "success" && "✅ "}
+                      {buildLog.level === "info"
+                        ? `[${buildLog.message}]`
+                        : buildLog.message}
+                    </pre>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 });
