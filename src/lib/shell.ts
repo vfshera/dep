0978f4da5dc85664cbc300b362e50b7ba7bdeae3
Franklin Shera <fshera96@gmail.com>
2024-07-03 17:04:01 +0300

    update shell

--- src/lib/shell.ts
+++ src/lib/shell.ts
@@ -42,92 +42,92 @@ export async function executeCommand(command: string): Promise<
 /**
  * Executes the "touch" command on the specified path.
  *
- * @param {string} path - the path to create a new file at
- * @return {Promise<void>} a Promise that resolves after creating the file
+ * @param {string} path - The path to create a new file at
+ * @return {Promise<{ ok: boolean, stdout?: string, error?: string }>} - The result of creating the file
  */
 export async function touch(path: string) {
-  return exec(`touch ${path}`);
+  return executeCommand(`touch ${path}`);
 }
 
 /**
  * Generate a listing of the specified directory.
  *
- * @param {string} dir - the directory to list
- * @return {Promise<string>} the result of listing the directory
+ * @param {string} dir - The directory to list
+ * @return {Promise<{ ok: boolean, stdout?: string, error?: string }>} - The result of listing the directory
  */
 export async function ls(dir: string) {
-  return exec(`ls ${dir}`);
+  return executeCommand(`ls ${dir}`);
 }
 
 /**
  * Executes the 'cat' command on the specified file path.
  *
- * @param {string} path - the path of the file to be read
- * @return {Promise<string>} a promise that resolves with the content of the file
+ * @param {string} path - The path of the file to be read
+ * @return {Promise<{ ok: boolean, stdout?: string, error?: string }>} - The content of the file
  */
 export async function cat(path: string) {
-  return exec(`cat ${path}`);
+  return executeCommand(`cat ${path}`);
 }
 
 /**
  * Generate a new directory at the specified path.
  *
- * @param {string} path - the path where the new directory will be created
- * @return {Promise<void>} a Promise that resolves after creating the directory
+ * @param {string} path - The path where the new directory will be created
+ * @return {Promise<{ ok: boolean, stdout?: string, error?: string }>} - The result of creating the directory
  */
 export async function mkdir(path: string) {
-  return exec(`mkdir ${path}`);
+  return executeCommand(`mkdir ${path}`);
 }
 
 /**
  * Change the current working directory to the specified directory.
  *
- * @param {string} dir - the directory to change to
- * @return {Promise<void>} a promise that resolves when the directory change is complete
+ * @param {string} dir - The directory to change to
+ * @return {Promise<{ ok: boolean, stdout?: string, error?: string }>} - The result of changing the directory
  */
 export async function cd(dir: string) {
-  return exec(`cd ${dir}`);
+  return executeCommand(`cd ${dir}`);
 }
 
 /**
  * Executes the "pwd" command asynchronously.
  *
- * @return {Promise<string>} The current working directory path.
+ * @return {Promise<{ ok: boolean, stdout?: string, error?: string }>} - The current working directory path
  */
 export async function pwd() {
-  return exec("pwd");
+  return executeCommand("pwd");
 }
 
 /**
  * Copies a file from one location to another.
  *
- * @param {string} from - the source file path
- * @param {string} to - the destination file path
- * @return {Promise<void>} a Promise that resolves when the file is successfully copied
+ * @param {string} from - The source file path
+ * @param {string} to - The destination file path
+ * @return {Promise<{ ok: boolean, stdout?: string, error?: string }>} - The result of copying the file
  */
 export async function cp(from: string, to: string) {
-  return exec(`cp ${from} ${to}`);
+  return executeCommand(`cp ${from} ${to}`);
 }
 
 /**
  * Removes a file or directory at the specified path.
  *
- * @param {string} path - the path of the file or directory to be removed
- * @return {Promise<void>} a promise that resolves after the file or directory is removed
+ * @param {string} path - The path of the file or directory to be removed
+ * @return {Promise<{ ok: boolean, stdout?: string, error?: string }>} - The result of removing the file or directory
  */
 export async function rm(path: string) {
-  return exec(`rm ${path}`);
+  return executeCommand(`rm ${path}`);
 }
 
 /**
  * Move a file or directory from one location to another.
  *
- * @param {string} from - the source path
- * @param {string} to - the destination path
- * @return {Promise<string>} - a promise that resolves with the output of the `mv` command
+ * @param {string} from - The source path
+ * @param {string} to - The destination path
+ * @return {Promise<{ ok: boolean, stdout?: string, error?: string }>} - The result of moving the file or directory
  */
 export async function mv(from: string, to: string) {
-  return exec(`mv ${from} ${to}`);
+  return executeCommand(`mv ${from} ${to}`);
 }
 
 /**
